27c671bbad239097d15f853823156ab12de7e21c
andreasLojdahl <andreas.lojdahl@gmail.com>
2023-05-07 20:51:21 +0200

    added views and routing

---
+++ src/views/GameBoard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+onMounted(() => {
+  //const numberOfplayers = route.params.numberOfPlayers
+})
+</script>
+<template>GameBoard</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
 
-<template>Home</template>
+const router = useRouter()
+
+const maxPlayers = 8
+const gameName = 'Random Roll'
+</script>
+
+<template>
+  <div class="homeView">
+    <p>{{ gameName }}</p>
+
+    <button
+      v-for="n in maxPlayers"
+      @click="() => router.push({ name: 'GameBoard', params: { numberOfPlayers: n + 1 } })"
+    >
+      {{ n + 1 }}
+    </button>
+  </div>
+</template>
+
+<style>
+.homeView {
+  display: flex;
+  flex-direction: column;
+}
+</style>
