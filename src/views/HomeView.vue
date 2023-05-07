8f9e818647db7b4ff08589557d9c9837bdf94982
andreasLojdahl <andreas.lojdahl@gmail.com>
2023-05-07 22:05:20 +0200

    feature: added styling to homwview and removed unused css classes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,26 +3,64 @@ import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const maxPlayers = 8
-const gameName = 'Random Roll'
+const maxPlayers = 9
+const gameNameHeader = 'Random Roll'
+const subHeader = 'Choose how many players'
 </script>
 
 <template>
   <div class="homeView">
-    <p>{{ gameName }}</p>
+    <p class="header courier-font-center">{{ gameNameHeader }}</p>
+    <p class="subHeader courier-font-center">{{ subHeader }}:</p>
 
-    <button
-      v-for="n in maxPlayers"
-      @click="() => router.push({ name: 'GameBoard', params: { numberOfPlayers: n + 1 } })"
-    >
-      {{ n + 1 }}
-    </button>
+    <div class="gridContainer">
+      <button
+        class="button"
+        v-for="n in maxPlayers"
+        @click="() => router.push({ name: 'GameBoard', params: { numberOfPlayers: n + 1 } })"
+      >
+        {{ n + 1 }}
+      </button>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 .homeView {
   display: flex;
   flex-direction: column;
+}
+.gridContainer {
+  height: 50vh;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px 20px;
+}
+.header {
+  color: white;
+  font-size: xx-large;
+  margin-bottom: 1rem;
+}
+.subHeader {
+  color: white;
+  font-size: large;
+  margin-bottom: 3rem;
+}
+.button {
+  font-size: xx-large;
+  font-family: Courier;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 10px;
+}
+.button:hover {
+  background-color: black;
+  color: white;
+  border: 2px solid white;
+}
+
+.courier-font-center {
+  font-family: Courier;
+  text-align: center;
 }
 </style>
